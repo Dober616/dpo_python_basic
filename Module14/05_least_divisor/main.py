d893c9ab782cc93e4982da12f9274b44cffcd658
Dober616 <Druz_kirill@icloud.com>
2022-10-19 22:47:03 +0200

    Merge remote-tracking branch 'origin/master'

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def gsd(number):
+    for i in range (2, number+1):
+        if number % i == 0:
+            gsd = i
+            break
+    return gsd
+number = int(input('Введите число: '))
+if number <= 1:
+    number = int(input('Число должно быть больше 1. Введите число повторно: '))
+print(gsd(number))
