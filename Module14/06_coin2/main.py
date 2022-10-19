cbb79166b47a786541c7655dad9f4b724c57d96b
Dober616 <Druz_kirill@icloud.com>
2022-10-19 23:11:02 +0200

    Merge remote-tracking branch 'origin/master'

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import math
+def coin(x,y):
+    l = math.sqrt(x**2 + y**2)
+    return l
+print('Введите координаты монетки')
+x = float(input('X: '))
+y = float(input('Y: '))
+radius = float(input('Введите радиус окружности: '))
+
+if abs(coin(x,y)) > radius:
+    print ('Монетки в области нет')
+else:
+    print('Монетка где-то рядом')
--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def number(x):
+    count = 0
+    a = x // 1000 % 10
+    b = x // 100 % 10
+    c = x // 10 % 10
+    d = x // 1 % 10
+    for digit in [a, b, c, d]:
+        for numm in [a, b, c, d]:
+            if digit == numm:
+                count += 1
+            else:
+                count = count
+        if count >= 3:
+            break
+        else:
+            break
+    return count
+
+
+first_numm = int(input('Введите первое число: '))
+last_numm = int(input('Введите второе число: '))
+for x in range(first_numm, last_numm):
+    if number(x) >= 3:
+        print (x)
