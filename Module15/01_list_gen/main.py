91379185d559d2df41365d012be4c00cbe5ccd82
Dober616 <Druz_kirill@icloud.com>
2022-10-23 19:08:12 +0200

    Первое выполнено

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+numbers_list = []
+number = int(input('До какого числа: '))
+for i in range (1, number+1, 2):
+    numbers_list.append(i)
+print('Список из нечетных чисел от 1 до ', 'N', ':', numbers_list)
