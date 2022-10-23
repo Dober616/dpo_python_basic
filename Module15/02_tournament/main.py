5f5b1bb64a9825337dbeb4cb414cfefdd9128c43
Dober616 <Druz_kirill@icloud.com>
2022-10-23 20:05:35 +0200

    Второе вроде сделал

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+team = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+new_team = []
+for name in team:
+    if (team.index(name) + 1) % 2 == 0:
+        new_team.append(name)
+print(new_team)
