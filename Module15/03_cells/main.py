274bbf5af0934f0aaf9cbd032b0b790abd797144
Dober616 <Druz_kirill@icloud.com>
2022-10-23 20:23:03 +0200

    с третьей справился

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+cells_efficency = []
+bad_cells = []
+sells_count = int(input('Количество клеток: '))
+for i in range (sells_count):
+    print('Эффективность ', i+1, 'клетки: ', end='')
+    efficency = int(input())
+    cells_efficency.append(efficency)
+for cell in  cells_efficency:
+    if cells_efficency.index(cell) + 1 > cell:
+        bad_cells.append(cell)
+print('Количество клеток: ', len(cells_efficency))
+print('Неподходящие значения: ', bad_cells)
