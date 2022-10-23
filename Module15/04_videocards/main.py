8e66739659fa429cec13f9e42ed33093465138bc
Dober616 <Druz_kirill@icloud.com>
2022-10-23 20:49:29 +0200

    4 готова

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,16 @@
-# TODO здесь писать код
+def top_cards(x):
+    max = 0
+    for i in x:
+        if i > max:
+            max = i
+    return (max)
+cards_count = int(input('Введите количество видеокарт: '))
+print('Количество видеокарт: ', cards_count)
+cards_list = []
+for _ in range (cards_count):
+    model = int(input('Видеокарта: '))
+    cards_list.append(model)
+cards_list.remove(top_cards(cards_list))
+print(cards_list)
 
-
+#Не совсем понятно, сколько последних карт удалять. 
