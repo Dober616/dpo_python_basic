b63aaf15f59d01691d57b71cbdff976b83cc0f3f
Dober616 <Druz_kirill@icloud.com>
2022-10-24 08:10:46 +0200

    temp

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+
+
+word = input('Введите слово: ')
+count = 0
+for letter in word:
+    for symbol in word:
+        if letter == symbol:
+            count +=1
+print(count)
