f61920ecf0d89e7904a4ab4c0e993c1860ad3fc1
Dober616 <Druz_kirill@icloud.com>
2022-10-24 14:28:36 +0200

    Восьмое готово

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def table(my_list):
+    for _ in range (move):
+        my_list = my_list[1:]+my_list[:1]
+    return my_list
+my_list = []
+new_list = []
+count_numbers = int(input('Введите количество элементов списка: '))
+for _ in range(count_numbers):
+    number = input('Введите число: ')
+    my_list.append(number)
+move = int(input('Сдвиг: '))
+print(my_list)
+print(table(my_list))
