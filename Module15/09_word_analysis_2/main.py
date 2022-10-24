4cfc0541e033c8dc688a576fef7d8902e6ca4ff8
Dober616 <Druz_kirill@icloud.com>
2022-10-24 15:10:11 +0200

    Залепуха какая-то получилась

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+lenght = len(word)
+x = 0
+y = lenght-1
+count = 0
+while x <= y:
+    if word[x] != word[y]:
+        print('Слово не является полиндромом')
+        break
+    else:
+        x += 1
+        y -= 1
+        count += 1
+    if count == lenght // 2:
+        print('Слово является полиндромом')
