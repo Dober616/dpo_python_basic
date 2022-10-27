8fe930b3638eefe9c7b10c6e42763f68f5395a16
Dober616 <Druz_kirill@icloud.com>
2022-10-27 15:06:41 +0200

    ttt

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,9 +1,9 @@
 def streamline():
-    for _ in range (10):
+    for _ in range (count_numm):
         for i in my_list:
             for y in my_list:
                 if i > y:
-                    my_list.insert(len(my_list)-1, i)
+                    my_list.insert(y, i)
                     my_list.remove(i)
                 elif i <= y:
                     my_list.insert(i, y)
@@ -15,7 +15,6 @@ my_list = []
 for _ in range(count_numm):
     number = int(input('Введите число: '))
     my_list.append(number)
-print(my_list)
 print(my_list)
 
 
