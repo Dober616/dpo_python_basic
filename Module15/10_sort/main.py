b5dde767058876e4d40ebae1b6e86ad8c6d30fb9
Dober616 <Druz_kirill@icloud.com>
2022-10-27 17:39:56 +0200

    победил наконец

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,21 +1,14 @@
 def streamline():
-    for _ in range (count_numm):
-        for i in my_list:
-            for y in my_list:
-                if i > y:
-                    my_list.insert(y, i)
-                    my_list.remove(i)
-                elif i <= y:
-                    my_list.insert(i, y)
-                    my_list.remove(y)
+    for _ in range (count_numm-1):
+        for i in range(count_numm-1):
+            if my_list[i] > my_list[i + 1]:
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
     return my_list
-
 count_numm = int(input('Сколько чисел в списке: '))
 my_list = []
 for _ in range(count_numm):
-    number = int(input('Введите число: '))
+    number = input('Введите число: ')
     my_list.append(number)
 print(my_list)
-
 
 print(streamline())
