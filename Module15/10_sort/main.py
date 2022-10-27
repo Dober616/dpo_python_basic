87f4491700764c3c5ee7f59eb36c690aec62546a
Dober616 <Druz_kirill@icloud.com>
2022-10-27 13:38:15 +0200

    Работает, но не всегда корректно

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,9 +1,8 @@
 def streamline():
-    for _ in range (count_numm):
-        for i in my_list:
-            for y in my_list:
-                if i > y:
-                    my_list.insert(len(my_list)-1, i)
+    for i in my_list:
+        for y in my_list:
+            if i > y:
+                my_list.insert(len(my_list)-1, i)
     return my_list
 count_numm = int(input('Сколько чисел в списке: '))
 my_list = []
