0be83c9c923026427da25c761aa08cba71a63fb1
druz_kirill <Druz_kirill@icloud.com>
2022-11-01 20:30:59 +0200

    party is over

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,12 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
-
-# TODO здесь писать код
+guest = 0
+while guest != 'пора спать':
+    print('Сейчас на вечеринке', len(guests), 'человек', guests)
+    guest = input('Гость пришел или ушел?: ')
+    if guest == 'пришел':
+        name = input('Имя гостя: ')
+        guests.append(name)
+    elif guest == 'ушел':
+        name = input('Имя гостя: ')
+        guests.remove(name)
+print('Вечеринка закончилась, все легли спать')
