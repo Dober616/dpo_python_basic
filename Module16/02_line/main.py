47a40610220c6f91a32a46d79a3b231492063a05
druz_kirill <Druz_kirill@icloud.com>
2022-11-01 20:09:39 +0200

    шеренга

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+general_list = []
+first_class_list = []
+second_class_list = []
+for height in range(160,177,2):
+    first_class_list.append(height)
+for height in range(162,181,3):
+    second_class_list.append(height)
+first_class_list.extend(second_class_list)
+first_class_list.sort()
+print('Отсортированный список учеников: ',first_class_list)
