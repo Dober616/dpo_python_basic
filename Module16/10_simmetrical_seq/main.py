39ec8772d49b83696a754343de79bb03347493f5
druz_kirill <Druz_kirill@icloud.com>
2022-11-07 11:35:19 +0200

    10 вроде работает

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+count_numbers = int(input('Количество чисел: '))
+subsequence = []
+for _ in range(count_numbers):
+    number = int(input('Число: '))
+    subsequence.append(number)
+add_list = []
+y = 1
+for digit in subsequence:
+    last_numm = subsequence[-y]
+    if digit == last_numm:
+        y += 1
+    else:
+        add_list.insert(0, digit)
+if len(add_list) > 0:
+    print('Последовательность: ', subsequence, '\nНужно приписать чисел: ', len(add_list), '\nСами числа: ', add_list)
