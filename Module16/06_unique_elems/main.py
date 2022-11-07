4384780299e2b71851f1b31e08fedba2708eb21d
druz_kirill <Druz_kirill@icloud.com>
2022-11-07 14:08:28 +0200

    10 вроде работает

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,21 +1,25 @@
-first_list = [1, 2, 3]
-sec_list = [2, 4, 6, 3, 3, 2, 1]
-new_list = []
-# for i in range(3):
-#     print('Введите', i+1, 'число для первого списка: ', end='')
-#     number = int(input())
-#     first_list.append(number)
-# for i in range(7):
-#     print('Введите', i+1, 'число для второго списка: ', end='')
-#     number = int(input())
-#     sec_list.append(number)
+def numm_remove(my_list):
+    my_list.remove(x)
+    return my_list
+
+
+first_list = []
+sec_list = []
+for i in range(3):
+    print('Введите', i+1, 'число для первого списка: ', end='')
+    number = int(input())
+    first_list.append(number)
+for i in range(7):
+    print('Введите', i+1, 'число для второго списка: ', end='')
+    number = int(input())
+    sec_list.append(number)
+print(first_list)
+print(sec_list)
+
 first_list.extend(sec_list)
-new_list.extend(first_list)
-print('Первый лист', first_list)
-for number in first_list:
-    for i in new_list:
-        if i == number:
-            first_list.remove(number)
-            break
-        new_list.remove(i)
-print('Новый список с уникальными эелементами:', first_list)
+print(first_list)
+for i in range(1, len(first_list)//2):
+    x = first_list[len(first_list)-i]
+    for _ in range(first_list.count(x)-1):
+        numm_remove(first_list)
+print('Новый список с уникальными элементами: ', first_list)
