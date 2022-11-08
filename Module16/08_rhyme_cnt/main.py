9be29664aa20abde6cc278ef837ebca432cd2cd3
druz_kirill <Druz_kirill@icloud.com>
2022-11-08 13:29:35 +0200

    Наконец-то 8 победил

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,28 +1,34 @@
-# def drop_numm():
-#     number = remove_index + (count_number - 1) - len(people_round)
-#     while number > len(people_round):
-#         number -= len(people_round)
-#     return number
+def list_remove():
+    people_round.pop(drop_number - 1)
+    return people_round
 
-# people_count = 5  # int(input('Количество человек: '))
-count_number = 7  # int(input('Какое число в считалке: '))
-people_round = ['a', 'b', 'c', 'd', 'e']
-# people_round = []
+people_count = int(input('Количество человек: '))
+count_number = int(input('Какое число в считалке: '))
+people_round = []
 print('Значит выбывает каждый', count_number, '- й человек', '\n++++++++++++++++++++++++++++++++++++')
-# for i in range(1, people_count+1):
-#     people_round.append(i)
-# print(people_round)
-
+for i in range(1, people_count+1):
+    people_round.append(i)
+print(people_round)
 
 start_index = 0
 
 while len(people_round) != 1:
     print('Текущий круг: ', people_round)
-    print('Начало счета с номера: ', people_round[start_index])
     drop_number = start_index + count_number
     while drop_number > len(people_round):
         drop_number -= len(people_round)
-    people_round.pop(drop_number-1)
-    start_index = drop_number
-    print('Выбывает человек под номером: ', people_round[drop_number-1])
+    print('Начало счета с номера: ', people_round[start_index])
+    print('Выбывает человек под номером: ', people_round[drop_number - 1])
+    print('=================================')
+    list_remove()
+
+    if drop_number == len(people_round):
+        start_index = 0
+    elif drop_number > len(people_round):
+        start_index = len(people_round) - (drop_number - 1)
+    else:
+        start_index = drop_number-1
+
+print('Остался человек под номером: ', people_round[0])
+
 
