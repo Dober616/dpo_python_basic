6e7e5f4db9a98c40574f652de6ecc959ad935b1f
Dober616 <Druz_kirill@icloud.com>
2022-11-08 11:50:35 +0200

    try

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,25 +1,28 @@
-def drop_numm():
-    number = numm_index + (count_number - 1) - len(people_round)
-    while number > len(people_round):
-        number -= len(people_round)
-    return number
+# def drop_numm():
+#     number = remove_index + (count_number - 1) - len(people_round)
+#     while number > len(people_round):
+#         number -= len(people_round)
+#     return number
 
-people_count = 5  # int(input('Количество человек: '))
+# people_count = 5  # int(input('Количество человек: '))
 count_number = 7  # int(input('Какое число в считалке: '))
-people_round = []
+people_round = ['a', 'b', 'c', 'd', 'e']
+# people_round = []
 print('Значит выбывает каждый', count_number, '- й человек', '\n++++++++++++++++++++++++++++++++++++')
-for i in range(1, people_count+1):
-    people_round.append(i)
+# for i in range(1, people_count+1):
+#     people_round.append(i)
+# print(people_round)
 
-numm_index = 0
+
+start_index = 0
+
 while len(people_round) != 1:
-    print('Текущий круг людей: ', people_round)
-    print('Начало счета с номера: ', people_round[drop_numm()-1])
-    if drop_numm() > len(people_round):
-        numm_index = drop_numm()-len(people_round)
-    else:
-        numm_index = drop_numm()
-    print('Выбывает номер: ', drop_numm())
-    print()
-    people_round.pop(drop_numm()-1)
-print('Остался человек под номером: ', people_round[0])
+    print('Текущий круг: ', people_round)
+    print('Начало счета с номера: ', people_round[start_index])
+    drop_number = start_index + count_number
+    while drop_number > len(people_round):
+        drop_number -= len(people_round)
+    people_round.pop(drop_number-1)
+    start_index = drop_number
+    print('Выбывает человек под номером: ', people_round[drop_number-1])
+
