1944041f0c9fe27ac51c34b02ddbf5f3b4204c69
druz_kirill <Druz_kirill@icloud.com>
2022-11-09 18:28:25 +0200

    fours is ready

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,13 @@
 alphabet = 'abcdefg'
+print('Копия строки: \t\t\t\t', alphabet[:])
+print('В обратном порядке: \t\t', alphabet[::-1])
+print('Каждый второй элемент: \t\t', alphabet[::2])
+print('Каждый второй после первого:', alphabet[1::2])
+print('Все элементы до второго: \t', alphabet[:2])
+print('Все с конца до предпосл.: \t', alphabet[:0:-1])
+print('В диапаз. от 3 до 4(без 4): ', alphabet[2:3])
+print('Последние три элемента : \t', alphabet[:3:-1])
+print('Все в диапазоне от 3 до 4: \t', alphabet[2:4])
+print('То же в обратном порядке: \t', alphabet[4:2:-1])
 
-# TODO здесь писать код
+
