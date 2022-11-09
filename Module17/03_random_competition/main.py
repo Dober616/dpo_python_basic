ed776227a199e7852795b39302aab5bd825e41a0
druz_kirill <Druz_kirill@icloud.com>
2022-11-09 17:56:19 +0200

    third so-so

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import random
+first_team = [round(random.uniform(5, 10), 2) for _ in range(10)]
+secnd_team = [round(random.uniform(5, 10), 2) for _ in range(10)]
+
+print('Первая команда: ', first_team)
+print('Вторая команда: ', secnd_team)
+winners = [(first_team[x] if first_team[x] > secnd_team[x] else secnd_team[x]) for x in range(10)]
+print('Победители тура: ', winners)
