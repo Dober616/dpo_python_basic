aa1062fda0c868db59de67fdecfc6f17ae3db745
druz_kirill <Druz_kirill@icloud.com>
2022-11-09 17:10:30 +0200

    first task ready

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+my_text = input('Введите текст: ')
+
+vowel = 'ауоыиэяюёе'
+
+vowel_list = [x for x in my_text for letter in vowel if x == letter]
+print('Список гласных букв: ', vowel_list, '\nДлина списка: ', len(vowel_list))
