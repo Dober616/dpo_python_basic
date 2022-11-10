0e01133f9f1edb62cdcc965421f08dd8ee256571
druz_kirill <Druz_kirill@icloud.com>
2022-11-10 12:07:59 +0200

    6 залепуха, но работает

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+n = int(input('Количество чисел в списке: '))
+my_list = [random.randint(0, 2) for _ in range(n)]
+cut_list = [numm  for numm in my_list if numm != 0]
+reaped_list = cut_list + [0] * (len(my_list) - len(cut_list))
+
+print(my_list)
+print(reaped_list)
+print(cut_list)
