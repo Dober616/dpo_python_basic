57bfdf1b89ad6ef0ecdf152e9d81059b54335c88
druz_kirill <Druz_kirill@icloud.com>
2022-11-11 16:39:29 +0200

    7 вроде работает

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,4 +1,5 @@
 n = int(input('Количество чисел: '))
 numm_in_list = int(input('Сколько чисел в посдсписке: '))
-my_list = [[number for number in range(i, n + 1, n // numm_in_list)] for i in range(1, n // numm_in_list + 1)]
+my_list = [[number for number in range(i, n + 1, n // numm_in_list)]
+           for i in range(1, n // numm_in_list + 1)]
 print('Двуменрый список: ', my_list)
