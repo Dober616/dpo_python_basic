9af7b30105200e8227a834e16c05fe190805dc72
druz_kirill <Druz_kirill@icloud.com>
2022-11-12 20:38:45 +0200

    9 победил

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+text = input('Введите сообщение: ')
+alfabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+shift = int(input('Введите сдвиг: '))
+encrypted_message = [alfabet[alfabet.index(letter) - len(alfabet) + shift] if letter in alfabet else letter for letter in text]
+
+print(''.join(encrypted_message))
+# В общем как-то так получилось...
