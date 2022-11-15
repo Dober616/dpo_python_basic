d110d33424eda455c8324ac01faa9606e371904a
Dober616 <Druz_kirill@icloud.com>
2022-11-15 15:53:13 +0200

    3 in work

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+forbidden_symbols = '@№$%^&\*()'.split(',')
+allowed_formats = '.txt, .docx'
+file_name = input('Введите название файла: ')
+if file_name.startswith():
+    print('Внимание, запрещенные символы!')
+elif file_name.endswith(allowed_formats):
+    print('Внимание, запрещенный формат')
+else:
+    print('Все ок')
