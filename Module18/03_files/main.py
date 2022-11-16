a458324cdcd48c2bfe7e684c387909dd6fa32bb2
druz_kirill <Druz_kirill@icloud.com>
2022-11-16 09:27:33 +0200

    Второе победил

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,9 +1,11 @@
-forbidden_symbols = '@№$%^&\*()'.split(',')
-allowed_formats = '.txt, .docx'
+forbidden_symbols = '@№$%^&\*()'
+allowed_formats = '.txt .docx'
 file_name = input('Введите название файла: ')
-if file_name.startswith():
-    print('Внимание, запрещенные символы!')
-elif file_name.endswith(allowed_formats):
-    print('Внимание, запрещенный формат')
+
+if file_name.startswith(tuple(forbidden_symbols)) == True:
+    print('Ошибка: навание начинается на один из запрещенных символов.')
+elif file_name.endswith(tuple(allowed_formats)) == False:
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
 else:
-    print('Все ок')
+    print('Файл назван верно')
+
