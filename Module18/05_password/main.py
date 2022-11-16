61cb2a4df84374b3f3b5e9d91517644fbd98ccc2
druz_kirill <Druz_kirill@icloud.com>
2022-11-16 10:37:36 +0200

    С паролями разобрались

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+password = input('Придумайте пароль: ')
+if len(password) < 8:
+    print('Пароль слишком короткий')
+elif sum(map(str.isupper, password)) < 1:
+    print('Мало заглавных букв')
+elif sum(map(str.isalnum, password)) < 3:
+    print('Нужно хотя бы 3 цифры')
+else:
+    print('Это надежный пароль!')
