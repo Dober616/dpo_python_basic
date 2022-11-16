69848de93322f3c28752054fe0479e86373b8021
druz_kirill <Druz_kirill@icloud.com>
2022-11-16 15:44:38 +0200

    6 готово

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import itertools
+string = 'aaaaabbcaaA'  # input('Введите строку: ')
+new_string = []
+
+for litter, group in itertools.groupby(string):
+    new_string.append(litter)
+    new_string.append(str([i for i in group].count(litter)))
+
+print(''.join(new_string))
