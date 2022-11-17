85fce07677dd51e44725fbcd7a3249aaa93ae79c
druz_kirill <Druz_kirill@icloud.com>
2022-11-17 11:07:10 +0200

    С 9 справился

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def punctuation(word):
+    for symbol in word:
+        if symbol.isalpha():
+            pass
+        else:
+            return symbol
+    return True
+
+message = 'Это задание очень! простое.'.split(' ')
+
+encrypted_message = [x[::-1] if punctuation(x) == True else x[-2:0:-1] + x[-1] for x in message]
+print(' '.join(encrypted_message))
+
