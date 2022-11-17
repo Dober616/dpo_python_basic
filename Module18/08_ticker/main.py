c24d6a33bc9a3f0bbcf5cc1581a243c95a4f0d81
druz_kirill <Druz_kirill@icloud.com>
2022-11-17 09:44:22 +0200

    8 не знаю, можно ли так

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def new_string(temp_string, i):
+    temp = temp_string[i:len(temp_string) + 1] + temp_string[0:i]
+    return temp
+first_string = 'abcd'
+second_string = 'cdab'
+shift = 0
+for i in range(4):
+    if first_string == new_string(second_string, i):
+        print('Первая строка получается из второй со сдвигом', shift)
+    else:
+        shift += 1
