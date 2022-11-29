e4e69ec1dc97fdb77d512082b355af3639d1ea1d
Dober616 <Druz_kirill@icloud.com>
2022-11-29 20:16:53 +0200

    2 наконец-то победил

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,20 +1,25 @@
-print('Количество стран', end=': ')
-country_count = int(input())
-countries = dict()
-for i in range(2):
-    print('{0} страна'.format(i + 1), end=': ')
-    cities = input()
-    countries_data = cities.split()
-    countries[countries_data[0]] = []
-    for citi in countries_data[1:]:
-        countries[countries_data[0]].append(citi)
+def citi_search():
+    for country in result:
+        if citi in result[country]:
+            return country
+    return False
 
-for i in range(3):
-    print(i + 1, '-й город', end=': ')
-    new_citi = input()
-    if new_citi in countries.values():
-        print('Город {0} расположен в стране'.format(new_citi))
+
+count = int(input('Количество стран: '))
+result = dict()
+for i in range(count):
+    data = input(f'{i+1}-я страна: ').split()
+    result[data[0]] = []
+    for j in data[1:]:
+        result[data[0]].append(j)
+
+for attemp in range(3):
+    citi = input(f'{attemp + 1}-й город: ')
+    if citi_search():
+        print(f'Город {citi} расположен в стране в {citi_search()}')
     else:
-        print('Города {0} в списке нет'.format(new_citi))
-print(countries.values())
-print(countries)
+        print(f'По городу {citi} данных нет')
+
+
+# Казахстан Астана Павлодар Кустанай
+# Россия Калининград Ростов Москва
