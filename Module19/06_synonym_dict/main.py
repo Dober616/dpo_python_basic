4dd2df9debdc0ad1a841a4f3db469bfce19c4892
druz_kirill <Druz_kirill@icloud.com>
2022-11-30 19:03:03 +0200

    В 6 проблема с проверкой без учета регистра

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+couples_words = int(input('Введите количество пар слов: '))
+sinonim_dict = dict()
+for i in range(couples_words):
+    word = input(f'{i + 1}-я пара: ').split(' - ')
+    sinonim_dict[word[0]] = word[1]
+
+print(sinonim_dict.up)
+my_word = input('Введите слово: ')
+if my_word in sinonim_dict.keys():
+    print(f'Синоним слова {my_word}: {sinonim_dict[my_word]}')
+else:
+    while my_word not in sinonim_dict.keys():
+        my_word = input('Такого слова нет \nВведите другое: ')
+    print(f'Синоним слова {my_word}: {sinonim_dict[my_word]}')
+
+# Завис на проверке без учета регистра. Не могу найти как уменьшить все буквы в справочнике
