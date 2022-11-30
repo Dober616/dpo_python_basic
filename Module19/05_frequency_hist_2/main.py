61a4a8f3f1aba967710464120136d4e301db913e
druz_kirill <Druz_kirill@icloud.com>
2022-11-30 16:04:59 +0200

    Наконец-то 4

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def dictionary():
+    for symb in text:
+        if not symb in symb_list:
+            symb_list[symb] = 1
+        else:
+            symb_list[symb] += 1
+    return symb_list
+
+
+symb_list = dict()
+text = input('Введите текст: ')
+symb_list.update(dictionary())
+print('Инвертированный словарь частот: ')
+new_dict = dict()
+for i in set(symb_list.values()):
+    new_dict[i] = []
+for letter in symb_list.keys():
+    new_dict[symb_list[letter]].append(letter)
+for count in new_dict:
+    print(f'{count}: {new_dict[count]}')
+
