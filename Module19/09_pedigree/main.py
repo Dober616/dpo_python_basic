8285bea2654e51bbdd0ddbf97441b70027830235
druz_kirill <Druz_kirill@icloud.com>
2022-12-06 14:24:59 +0200

    Завис на 9

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def relative_count(name):
+
+    if name not in family_tree:
+        result[name] = 0
+        return 0
+    parent = family_tree[name]
+    if parent in result:
+        count = result[parent] + 1
+    else:
+        count = relative_count(parent) + 1
+        result[name] = count
+        return count
+
+people_count = int(input('Введите количество человек: '))
+family_tree = dict()
+for i in range(people_count):
+    child, parent = input('Введите ребенка и родителя: ').split()
+    family_tree[child] = parent
+
+family_list = set(family_tree.keys()|family_tree.values())
+result = dict()
+for name in family_list:
+    if name not in result:
+        relative_count(name)
+print(result)
+
+# for name in sorted(result):
+#     print('Высота каждого члена семьи: \n', name, result[name])
