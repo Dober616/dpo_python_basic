e0312bdd40a5bd2f6741721e0e8d8f63797d694b
Dober616 <Druz_kirill@icloud.com>
2022-12-06 15:31:03 +0200

    10 легко

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+my_string = input('Введите строку: ')
+mono_litters = set()
+for letter in my_string:
+    if letter in mono_litters:
+        mono_litters.remove(letter)
+    else:
+        mono_litters.add(letter)
+if len(mono_litters) > 1:
+    print('Нельзя сделать палиндром')
+else:
+    print('Можно сделать палиндром')
