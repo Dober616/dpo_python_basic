621987c60cf753ebda2050551e92d3cf8e142291
druz_kirill <Druz_kirill@icloud.com>
2022-12-06 10:45:30 +0200

    8 вроде как-то так

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import random
+mistery = input('Загадайте число: ')
+max_numm = int(input('Введите максимальное число: '))
+result = set()
+numm_list = set([random.randint(1, max_numm) for _ in range(3)])
+question = input(f'Нужное число есть среди вот этих чисел {numm_list}? \nОтвет Артема: ')
+while question != 'Помогите':
+    numm_list = set([random.randint(1, max_numm) for _ in range(3)])
+    question = input(f'Нужное число есть среди вот этих чисел {numm_list}? \nОтвет Артема: ')
+    if question == 'да':
+        result.update(numm_list)
+    else:
+        result.difference(numm_list)
+print('Артем мог загадать следующие числа: ', set(result))
