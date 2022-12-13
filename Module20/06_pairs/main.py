dabeb69387ccd2ba6ca467062380b1737bfe38ad
druz_kirill <Druz_kirill@icloud.com>
2022-12-13 12:33:47 +0200

    6 готово

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import random
+def pairs(origin_list):
+    first_list = []
+    secnd_list = []
+    for i, number in enumerate(origin_list):
+        if i % 2 == 0:
+            first_list.append(number)
+        else:
+            secnd_list.append(number)
+    return zip(first_list, secnd_list)
+
+
+origin_list = [random.randint(0, 10) for _ in range(10)]
+result_list = []
+print('Оригинальный список: ', origin_list)
+for i in pairs(origin_list):
+    result_list.append(i)
+print('Новый список: ', result_list)
