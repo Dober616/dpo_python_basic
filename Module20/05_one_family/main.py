90fe142d4933d71171f7650450db6d2e886f08a3
druz_kirill <Druz_kirill@icloud.com>
2022-12-13 11:55:54 +0200

    4 хз на сколько это эффективно, но вроде работает

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+family_dict = {
+    'Кирилл Друзьяк': 38,
+    'Катя Друзьяк': 33,
+    'Аня Романова': 34,
+    'Сергей Витолс': 35,
+    'Вадим Романов': 40,
+    'Олег Друзьяк': 60,
+    'Николай Виноградов': 83,
+}
+
+age_list = []
+some_surname = input('Введите фамилию: ')
+for name in family_dict.keys():
+    if some_surname in name:
+        age_list.append(name)
+
+for i in age_list:
+    print(i, family_dict[i])
