a32d7fb12b2ac29156cc08c4b6f2c1daf2bb5e10
druz_kirill <Druz_kirill@icloud.com>
2022-12-13 11:31:49 +0200

    3 вроде получилось

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def last_numm():
+    new_list = []
+    for index, numm in enumerate(slicer[0]):
+        if numm == 2:
+            new_list.append(index)
+    new_list = tuple(new_list)
+    if slicer[0].count(2) > 1:
+        return slicer[0][new_list[0]:new_list[1] + 1]
+    elif slicer[0].count(2) == 1:
+        return slicer[0][new_list[0]:]
+    else:
+        return ()
+
+
+slicer = ((1, 3, 4, 5, 6, 7, 8, 9, 10), 2)
+print('Ответ в консоли: ', last_numm())
