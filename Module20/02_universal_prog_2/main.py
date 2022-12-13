0f5565e25a7005de4c79fdbcc0fa0c1ad7486020
druz_kirill <Druz_kirill@icloud.com>
2022-12-13 08:46:02 +0200

    2 готово

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def is_prime(numm):
+    for i in range(2, numm + 1):
+        res = numm % i
+        if res == 0:
+            if i == numm:
+                return True
+            else:
+                return False
+
+my_list = 'О дивный новый мир!'
+new_list = []
+for index, numm in enumerate(my_list):
+    if is_prime(index):
+        new_list.append(numm)
+print(new_list)
