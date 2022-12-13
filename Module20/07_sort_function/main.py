b23ea432fffe66314b683a25d9561dc9cfb1b7b1
druz_kirill <Druz_kirill@icloud.com>
2022-12-13 12:46:50 +0200

    6 готово

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def sort_tupple():
+    for numm in my_tupple:
+        if numm != int(numm):
+            return my_tupple
+    return sorted(my_tupple)
+
+
+my_tupple = 6, 3, -1, 8, 4, 10, -5
+print(sort_tupple())
