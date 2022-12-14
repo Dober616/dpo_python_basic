679647f2a583a4edc0b9333b154175c0b6cea569
druz_kirill <Druz_kirill@icloud.com>
2022-12-14 08:54:29 +0200

    10 вроде так

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def my_zip():
+    result_line = []
+    if len(line_1) <= len(line_2):
+        short = len(line_1)
+    else:
+        short = len(line_2)
+    for i in range(short):
+        temp_tuple = line_1[i], line_2[i]
+        result_line.append(temp_tuple)
+    return result_line
+
+line_1 = ('abcd')
+line_2 = (10, 20, 30, 40)
+for i in my_zip():
+    print(i)
+
