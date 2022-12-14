3aad1b343375489e4bb5f02e4ad1620f03f1fd8f
druz_kirill <Druz_kirill@icloud.com>
2022-12-14 11:09:25 +0200

    21 модуль начало

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,9 +1,15 @@
-def min_len():
-    return min(len(my_string), len(my_tupple))
+def min_len(len_1, len_2):
+    return min(len(len_1), len(len_2))
 
-my_string = 'qwer'
-my_tupple = (10, 20, 30, 40)
 
-new_tupple = ((my_string[i], my_tupple[i]) for i in range(min_len()))
-for i in new_tupple:
+def my_zip(data_1, data_2):
+    new_tupple = ((data_1[i], data_2[i]) for i in range(min_len(my_string, my_tupple)))
+    return new_tupple
+
+
+
+my_string = 'qwer' # input('Введите строку')
+my_tupple = (10, 20, 30, 40) # input('Введите кортеж')
+
+for i in my_zip(my_string, my_tupple):
     print(i)
