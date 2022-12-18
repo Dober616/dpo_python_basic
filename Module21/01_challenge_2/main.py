12a25c69cda646dd6619863c1de5aac181a5dca8
druz_kirill <Druz_kirill@icloud.com>
2022-12-18 12:25:40 +0200

    1 готово.

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def none_cycle(some_numm):
+    if some_numm != 1:
+        none_cycle(some_numm - 1)
+    print(some_numm)
+
+
+
+numm = int(input('Введите num: '))
+
+none_cycle(numm)
+
