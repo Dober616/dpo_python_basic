ffc2f05d1d3b70330467118a0b89c43daaddc691
druz_kirill <Druz_kirill@icloud.com>
2022-12-25 10:05:53 +0200

    6 готово

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,5 @@
+import copy
+
 site = {
     'html': {
         'head': {
@@ -11,4 +13,19 @@ site = {
     }
 }
 
-# TODO здесь писать код
+
+site_count = int(input('Сколько сайтов: '))
+
+
+def change_product(site):
+    for _ in range(site_count):
+        product_name = input('Введите название продукта для нового сайта: ')
+        print(f'Сайт для {product_name}')
+        duplicate_site = copy.deepcopy(site)
+        duplicate_site['html']['head']['title'] = f'Куплю/продам {product_name} недорого'
+        duplicate_site['html']['body']['h2'] = f'У нас самая низкая цена на {product_name}'
+        print(duplicate_site)
+
+
+change_product(site)
+
