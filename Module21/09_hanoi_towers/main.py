f0ad0ca77ac3cff5f178aeeb23b5a98bc9148051
Dober616 <Druz_kirill@icloud.com>
2022-12-25 13:49:10 +0200

    9 работает

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def towers(n, first, last):
+    if n == 0:
+        return
+    temp = 6 - first - last
+    towers(n - 1, first, temp)
+    print(f'Перенести диск {n} со стержня {first} на стержень {last}')
+    towers(n - 1, temp, last)
+
+towers(3, 1, 3)
