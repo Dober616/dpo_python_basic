65d7bb0e29b2206966fbed6066306470e9f9852a
druz_kirill <Druz_kirill@icloud.com>
2022-12-25 10:45:57 +0200

    7 готово

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+def list_sum(my_list):
+    return sum(element if isinstance(element, int) else list_sum(element) for element in my_list)
+
+
+print(list_sum([1, 2, 3, [1, 2, [3, 4]]]))
