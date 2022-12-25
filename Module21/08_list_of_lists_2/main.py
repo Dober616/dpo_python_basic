617320a797d424003790dd049e2f7458cc26005e
druz_kirill <Druz_kirill@icloud.com>
2022-12-25 11:03:01 +0200

    8 в работе

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+def clean_list(some_list):
+    temp_list = []
+    for element in some_list:
+        if isinstance(element, list):
+            temp_list.append(clean_list(element))
+        else:
+            temp_list.append(element)
+    return temp_list
+    # return [element if isinstance(element, int) else clean_list(element) for element in some_list ]
+
+print(clean_list(nice_list))
