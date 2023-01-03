6366a4e9bf344b709832d570bea8730acc9947ff
druz_kirill <Druz_kirill@icloud.com>
2023-01-03 14:49:51 +0200

    1 задание. Еще доработатать

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+initial_file = open('numbers.txt', 'r')
+data = initial_file.read()
+numbers_list = []
+for line in data:
+    for symbol in line:
+        if symbol.isdigit():
+            numbers_list.append(symbol)
+result = 0
+for digit in numbers_list:
+    result += int(digit)
+
+final_data = open('answer.txt', 'w')
+final_data.write(str(result))
+initial_file.close()
+final_data.close()
