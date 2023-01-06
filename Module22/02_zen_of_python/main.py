f8cd46228cc3ac85144a80acacf2dae3a74139b3
druz_kirill <Druz_kirill@icloud.com>
2023-01-06 09:56:47 +0200

    2 работает

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+zen_python = open('zen.txt', 'r')
+data = zen_python.read()
+final_data = open('result.txt', 'w')
+for line in reversed(data.split('\n')):
+    final_data.write(f'{line}\n')
+zen_python.close()
+final_data.close()
+
