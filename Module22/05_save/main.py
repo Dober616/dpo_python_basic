25fcc959ba0b92fc67cf9558301faa8393a76607
druz_kirill <Druz_kirill@icloud.com>
2023-01-09 20:19:09 +0200

    5 с костылем, но работает

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+import os
+
+
+my_text = input('Введите строку: ')
+my_path = ' ' + input('Куда хотите сохранить документ? Введите последовательность папок (через пробел):')
+file_name = input('Введите имя файла: ')
+dir_path = os.path.join(my_path.replace(' ', '/'))
+file_path = os.path.join(dir_path, file_name)
+check_file = os.path.exists(file_path)
+if not check_file:
+    data = open(file_path, 'w')
+    data.write(my_text)
+    data.close()
+    print(f'Файл {file_name} успешно сохранен')
+else:
+    question = input('Такой файл уже существует. Вы действительно хотите перезаписать файл? (y/n): ')
+    if question == 'y':
+        data = open(file_path, 'w')
+        data.write(my_text)
+        data.close()
+        print('Данные успешно перезаписаны')
+    else:
+        print('Ну тогда ничего делать не будем')
+
+
+
+
+
