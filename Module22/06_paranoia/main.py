ca6e9b4fba9d95ee95716608e561ea93a063c58c
druz_kirill <Druz_kirill@icloud.com>
2023-01-09 21:44:54 +0200

    6 работает

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import os
+
+
+data = open(os.path.abspath('text.txt'), 'w')
+for i in range(4):
+    data.write('Hello\n')
+data.close()
+alfabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+read_data = open(os.path.abspath('text.txt'), 'r')
+write_data = open(os.path.abspath('cipher_text.txt'), 'w')
+shift = 1
+for string in read_data:
+    encrypted_string = [alfabet[alfabet.index(letter) - len(alfabet) + shift] if letter in alfabet else letter for letter in string]
+    write_data.write(''.join(encrypted_string))
+    shift += 1
+
