735f165090f1af0f562bb7cba045dcf53f045a99
druz_kirill <Druz_kirill@icloud.com>
2023-01-10 14:32:32 +0200

    8 задание проблема с итоговой сортировкой

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+import os
+import math
+
+letters = {}
+letters_frequency = {}
+alfabet = 'abcdefghijklmnopqrstuvwxyz'
+original_file = open(os.path.abspath('text.txt'), 'r')
+final_file = open(os.path.abspath('analysis.txt'), 'w')
+for string in original_file:
+    for letter in string:
+        if letter.lower() in alfabet:
+            if letter.lower() in letters:
+                letters[letter.lower()] += 1
+            else:
+                letters[letter.lower()] = 1
+print(letters)
+
+for letter in sorted(letters):
+    letters_frequency[letter] = round(letters[letter] / len(letters), 3)
+for element in letters_frequency:
+    final_file.write(f'{element} {letters_frequency[element]}\n')
+original_file.close()
+final_file.close()
+
+# никак не могу понять как отсортировать по 2 параметрам
