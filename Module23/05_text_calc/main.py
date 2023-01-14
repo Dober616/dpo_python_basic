ad80a10793fca42d7ec73b718e764fa0982bb819
druz_kirill <Druz_kirill@icloud.com>
2023-01-14 18:46:45 +0200

    5 вроде работает

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+result = 0
+
+with open('calc.txt', 'r') as text_file:
+    for i_line in text_file.readlines():
+        try:
+            result += eval(i_line)
+        except:
+            if input(f'Ошибка в строке ({i_line[:-1]}). Исправить? ') == 'да':
+                right_string = input('Введите исправленную строку: ')
+                result += eval(right_string)
+            else:
+                pass
+
+print(f'Сумма результатов: {result}')
