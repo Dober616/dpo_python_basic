4e151d97f77b810560d8b6c11057c0370f0984e5
druz_kirill <Druz_kirill@icloud.com>
2023-01-14 15:15:12 +0200

    3 не сделал(вероятность)

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+summ = 0
+with open('out_file.txt', 'w') as result_file:
+    while summ < 777:
+        numm = int(input('Введите число: '))
+        summ += numm
+        result_file.write(f'{str(numm)}\n')
+    print('Вы успешно выполнили условие для выхода из порочного цикла!')
