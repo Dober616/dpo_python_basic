82b861ec6e0b1e9744357c8f51b7c23298f03f43
druz_kirill <Druz_kirill@icloud.com>
2023-01-14 11:52:02 +0200

    1 готово

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+len_summ = 0
+string_count = 0
+try:
+    with open('people.txt', 'r') as people_file:
+        for name in people_file:
+            string_count += 1
+            lenght = len(name)
+            if name.endswith('\n'):
+                lenght -= 1
+            if lenght < 3:
+                raise BaseException
+            len_summ += lenght
+except FileNotFoundError:
+    print('Такой файл не найден.')
+except BaseException:
+    print(f'Длина строки {string_count} меньше 3 букв')
+finally:
+    print(f'Итоговая сумма символов: {len_summ}')
