2bb06bbad3d01744429d1176fa8162f6eb631e8c
druz_kirill <Druz_kirill@icloud.com>
2023-01-15 19:45:26 +0200

    work

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,7 +1,19 @@
+import random
+
+
+def is_exeptation_raise():
+    return random.randint(1, 13) == 7
+
+
 summ = 0
 with open('out_file.txt', 'w') as result_file:
     while summ < 777:
         numm = int(input('Введите число: '))
         summ += numm
+        try:
+            if is_exeptation_raise():
+                raise Exception
+        except Exception:
+            print('Сегодня не ваш день')
         result_file.write(f'{str(numm)}\n')
     print('Вы успешно выполнили условие для выхода из порочного цикла!')
