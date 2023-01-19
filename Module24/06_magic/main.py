176cad3b2e5f8be893618be0c633d936d397b2f3
druz_kirill <Druz_kirill@icloud.com>
2023-01-19 14:35:27 +0200

    6 вроде получилось

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,44 @@
-# TODO здесь писать код
+class Water:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return 'Шторм'
+        elif isinstance(other, Fire):
+            return 'Пар'
+        elif isinstance(other, Earth):
+            return 'Грязь'
+    def __str__(self):
+        return 'Вода'
+
+
+class Air:
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return 'Молния'
+        elif isinstance(other, Earth):
+            return 'Пыль'
+        elif isinstance(other, Water):
+            return 'Шторм'
+    def __str__(self):
+        return 'Воздух'
+
+
+class Fire:
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return 'Лава'
+    def __str__(self):
+        return 'Огонь'
+
+
+class Earth:
+    def __str__(self):
+        return 'Земля'
+
+
+
+water = Water()
+air = Air()
+earth = Earth()
+fire = Fire()
+
+print(water + air)
