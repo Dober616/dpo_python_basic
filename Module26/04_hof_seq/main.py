a5b3f0d47e90b98b1a81fbdc2ec8e63dcb524ac3
druz_kirill <Druz_kirill@icloud.com>
2023-02-03 11:20:19 +0200

    4 получилось

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def subsequence_q(q):
+    if not q == [1, 1]:
+        print('Передано неверное значение!')
+    else:
+        my_list = q[:]
+        while True:
+            try:
+                q = my_list[-my_list[-1]] + my_list[-my_list[-2]]
+                my_list.append(q)
+                yield q
+            except IndexError:
+                return
+
+
+for i, number in enumerate(subsequence_q([1, 1])):
+    if i <= 30:
+        print(number)
+    else:
+        break
