79027bff548406fb0f1dc97657e10fb5d5945702
druz_kirill <Druz_kirill@icloud.com>
2023-02-03 14:33:58 +0200

    5 вроде заработало

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import os
+
+
+def string_count():
+    count = 0
+    files = os.listdir()
+    for file in  files:
+        if file.rstrip('.py'):
+            with open(file, 'r') as my_file:
+                for line in my_file:
+                    count += 1
+        print(files)
+        return count
+
+print(string_count())
