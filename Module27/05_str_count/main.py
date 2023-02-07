fd25aa712dc612c7468ed8fa4d2a57b45abe692f
Dober616 <Druz_kirill@icloud.com>
2023-02-07 17:06:46 +0200

    4 готово

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+
+
+class Counter:
+    def __init__(self, count):
+        self.count = count
+
+
+class Func(Counter):
+    def __init__(self, func, count=0):
+        super().__init__(count)
+        self.func = func
+
+    def decorator(self):
+        def wrapped_func(*args, **kwargs):
+            return self.func()
+            self.count += 1
+
+
+        return wrapped_func
+
+
+
