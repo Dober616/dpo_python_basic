b060f8ea9488c754fdcb1f1e3182d757112f39c4
druz_kirill <Druz_kirill@icloud.com>
2023-02-07 12:21:01 +0200

    1 готово

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def how_are_you(func):
+    def wrapped_func():
+        question = input('Как дела? Пожалуйста ответьте: ')
+        result = func()
+        return result
+    return wrapped_func
+
+@how_are_you
+def test():
+    print('Тут что-то происходит...')
+
+test()
+
