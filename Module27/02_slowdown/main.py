61b024ed39abe779e06fab4af49cfddf6d2891d5
druz_kirill <Druz_kirill@icloud.com>
2023-02-07 12:31:10 +0200

    2 готово

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,20 @@
+import time
+import functools
 
+
+def time_freeze(funk):
+    @functools.wraps(funk)
+    def wrapped_func(*args, **kwargs):
+        print(f'Выполняется {funk.__name__}')
+        time.sleep(5)
+        result = funk(*args, **kwargs)
+        return result
+    return wrapped_func
+
+
+@time_freeze
+def page_refresh(site_name):
+    print(f'Страница сайта {site_name} обновлена')
+
+
+my_page = page_refresh('VK.com')
