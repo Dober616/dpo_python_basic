b583d2056709bbfae538c994b663a67c79cf0493
Dober616 <Druz_kirill@icloud.com>
2023-02-07 15:53:50 +0200

    4 готово

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import functools
+
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        print(f'Вызывается: {func.__name__}{args})\n'
+              f'{func.__name__} вернула значение {repr(func(*args))}')
+        print(func(*args, **kwargs))
+    return wrapped_func
+
+
+@debug
+def greeting(name, age=None):
+    if age:
+        return f'Привет, {name}! Тебе уже {age} лет! Как быстро ты растешь!\n'
+    else:
+        return f'Привет, {name}\n'
+
+
+greeting('Кирилл', 38)
+greeting('Егор')
+greeting('Катя', 16)
