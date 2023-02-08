4d24da88ea58927b09e5f3b0f6e586108799da19
druz_kirill <Druz_kirill@icloud.com>
2023-02-08 11:45:31 +0200

    5 получилось

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -6,9 +6,7 @@ def decorator(func):
     def wrapped_func(*args, **kwargs):
         func(*args, **kwargs)
         count_funk[func.__name__] += 1
-
-
-
+        print(f'Функцию {func.__name__} вызвали {count_funk[func.__name__]} раз')
     return wrapped_func
 
 
@@ -25,6 +23,6 @@ my_func()
 for i in range(5):
     ttt()
 
-
-for key in count_funk.keys():
-    print(f'Функция {key} вызывалась {count_funk[key]} раз')
+#
+# for key in count_funk.keys():
+#     print(f'Функция {key} вызывалась {count_funk[key]} раз')
