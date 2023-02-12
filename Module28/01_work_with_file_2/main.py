a206ae297431d89e57a432dad96dff3f13d0eb78
druz_kirill <Druz_kirill@icloud.com>
2023-02-12 10:03:07 +0200

    1 работает

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+class File:
+    def __init__(self, filename: str, mode: str):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        try:
+            self.file = open(self.filename, self.mode)
+            return self.file
+        except FileNotFoundError:
+            print('Такого файла нет, создаем новый')
+            self.file = open(self.filename, 'w')
+            self.file.write('Файла не было, поэтому создали новый, теперь можете делать с ним что хотите')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        return True
+
+with File('mmm.txt', 'r') as ttt:
+   for i in ttt:
+       print(i)
+
+
+
